54062f6d4a80967a0c30504246cd86901934403f
malekascha <malekascha@gmail.com>
2016-04-23 19:40:59 -0700

    updated view with circles for population visualization

--- public/cesium-code/app.js
+++ public/cesium-code/app.js
@@ -10,6 +10,7 @@ var clock = new Cesium.Clock({
 var viewer = new Cesium.Viewer('cesiumContainer', {
   clock: clock
 });
+var currentInterval;
 
 var intervals = [];
 for(var i = julianStart; i <= julianStop; i += 365){
@@ -23,9 +24,56 @@ for(var i = julianStart; i <= julianStop; i += 365){
 }
 var intervalCollection = new Cesium.TimeIntervalCollection(intervals);
 
+
+var redCircle = function(long,lat,pop,size,name) {
+   return {
+     position: Cesium.Cartesian3.fromDegrees(long, lat),
+     name : pop,
+     ellipse : {
+       semiMinorAxis : size,
+       semiMajorAxis : size,
+       height: 200000.0,
+       material : Cesium.Color.RED.withAlpha(0.5)
+     },
+     label: {
+      
+     }
+   };
+};
+
+var circleMaker = function(objArr) {
+ var popArr = [], long, lat, pop, size, name; 
+ for(var i = 0; i < objArr.length; i++) {
+  if(!objArr[i].long){
+    continue;
+  }
+   pop = objArr[i].population;
+   long = objArr[i].long;
+   lat = objArr[i].lat;
+   size = sizer(pop);
+   name = objArr[i].name;
+   popArr.push(viewer.entities.add(redCircle(long, lat, pop, size)));
+ }
+ return popArr;
+};
+
+var sizer = function(pop) {
+ var popLimit = 100000000, base = 100000;
+ return size = (pop/popLimit) * base;
+};
+
 var updateMap = function(e){
   var interval = intervalCollection.findIntervalContainingDate(viewer.clock.currentTime);
-  if(interval){
+  if(!(interval === currentInterval)){
+    viewer.entities.removeAll();
+    currentInterval = interval;
+    $.ajax({
+      url: '/data/population?year=2009',
+      success: function(res){
+        viewer.entities.add(circleMaker(res));
+      }
+    });
   }
 };
-viewer.clock.onTick.addEventListener(updateMap);  
+viewer.clock.onTick.addEventListener(updateMap); 
+
