731a77cdd3d69ba7ff07ee61425bada9fd2e73a0
Sergey Piterman <sergeypiterman@gmail.com>
2016-04-24 10:21:21 -0700

    [refactor] Fix filtering earthquake data by year

--- server/API-calls/queryEarthquake.js
+++ server/API-calls/queryEarthquake.js
@@ -10,17 +10,21 @@ module.exports = {
     const data = JSON.parse(json);
 
     for(var i = 1; i < data.length; i++){
-      let obj = {};
-      obj.name = data[i]['Place'];
-      obj.year = data[i][year];
-      obj.lat = data[i]['Latitude'];
-      obj.long = data[i]['Longitude'];
-      obj.deaths = data[i]['Deaths'];
-      obj.injuries = data[i]['Injuries'] || 0;
-      obj.month = data[i]['Mo'];
-      obj.day = data[i]['Dy'];
+      if(data[i]['Year'].toString() === year){
+        let obj = {};
 
-      results.push(obj);
+        obj.name = data[i]['Place'];
+        obj.year = data[i]['Year'];
+        obj.lat = data[i]['Latitude'];
+        obj.long = data[i]['Longitude'];
+        obj.deaths = data[i]['Deaths'];
+        obj.injuries = data[i]['Injuries'] || 0;
+        obj.month = data[i]['Mo'];
+        obj.day = data[i]['Dy'];
+        obj.hour = data[i]['Hr'];
+
+        results.push(obj);
+      }
     }
 
   return results;
