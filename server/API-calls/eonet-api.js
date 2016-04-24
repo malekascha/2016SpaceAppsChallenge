044b59ab2eda3515563591676ef9e499c66d1663
malekascha <malekascha@gmail.com>
2016-04-24 12:25:02 -0700

    added more filtering to eonetAPI

--- server/API-calls/eonet-api.js
+++ server/API-calls/eonet-api.js
@@ -18,8 +18,34 @@ module.exports = {
       if(err){
         console.log(err);
       } else {
-        route.send(body);
+        var data = JSON.parse(body);
+        var results = [];
+        for(var i = 0; i < data.events.length; i++){
+          let obj = {};
+          let event = data.events[i];
+          if(event.geometries.length > 1){
+            obj.type = 'line';
+          } else{
+            obj.type = event.geometries[0].type;
+          }
+          obj.geometries = [];
+          if(obj.type === 'Polygon'){
+            event.geometries[0].coordinates[0].forEach(function(item){
+              obj.geometries.push(item[0]);
+              obj.geometries.push(item[1]);
+            })
+          } else {
+            event.geometries.forEach(function(item){
+              obj.geometries.push(item.coordinates[0]);
+              obj.geometries.push(item.coordinates[1]);
+            });
+          }
+          obj.title = event.title;
+          obj.description = event.description;
+          results.push(obj);
+        }
+        route.send(JSON.stringify(results));
       }
-    })
+    });
   }
 }
